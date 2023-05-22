f9a36c43986ae2a937d3f9aea0d9feeda076017a
ZCrooks <zachthedevguy@gmail.com>
2023-05-22 19:28:56 -0400

    completed Header and Footer content

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Designed & Coded by Zachary Crooks with Juno College of Technology</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <h1>COIN WATCHER</h1> 
+    )
+}
+
+export default Header;
